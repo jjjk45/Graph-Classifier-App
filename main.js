9a84bbf477c7b7ad5bb431b7035fe88ae679de20
Jayden Ramirez <jaydenr0311@gmail.com>
2026-02-26 01:07:17 -0500

    OOP :)
    moved everything into a class named Graphy (must change this weak name), got the vertex drawing and deletion to be grid style, got some of the vertex and grid/canvas propertiess to be programmatically based on the vertex radius (in px)

--- main.js
+++ main.js
@@ -1,21 +1,47 @@
-
-function placeVertex(e, arr, ctx) {
-    arr.push({x: e.offsetX, y: e.offsetY});
-    drawVertex(e.offsetX, e.offsetY, ctx);
-    return;
-}
-/**
- * @param {mouseEvent/pointerEvent} e  //WHICH ONE?????
- * @param {canvas context} ctx
- */
-function drawVertex(x, y, ctx) {
-    ctx.beginPath();
-    ctx.arc(x, y, 7.5, 0, Math.PI*2, false);
-    ctx.lineWidth = 3;
-    ctx.strokeStyle = "#000";
-    ctx.fillStyle = "black";
-    ctx.fill();
-    ctx.stroke();
+class Graphy    {
+    /**
+     * @param {number} radius - vertex radius
+     */
+    constructor(radius, ctx) {
+        this.radius = radius;   //I like 7.5
+        this.gridSize = radius * 4;
+        this.cellHalf = this.gridSize / 2;
+        this.vertices = new Map();
+        this.ctx = ctx;
+    }
+    /**
+     * @param {mouseEvent/pointerEvent} event
+     * @param {hashmap} this.vertices
+     * @param {canvas context} ctx
+     */
+    clickCanvas(event)    {
+        let xCoord = Math.round(event.offsetX/this.gridSize) * this.gridSize;
+        let yCoord = Math.round(event.offsetY/this.gridSize) * this.gridSize;
+        let key = `${xCoord},${yCoord}`;
+        if(this.vertices.has(key))    {
+            console.log(`deleted (${key})`);
+            this.vertices.delete(key);
+            this.ctx.clearRect(xCoord - this.cellHalf, yCoord - this.cellHalf, this.gridSize, this.gridSize);
+        } else /*if(!this.vertices.has(key))*/{
+            console.log(`added (${key})`);
+            this.vertices.set(key, true);
+            this.#drawVertex(xCoord, yCoord);
+        }
+    }
+    /**
+     * @param {mouseEvent/pointerEvent} event  //WHICH ONE?????
+     * @param {canvas context} ctx
+     */
+    #drawVertex(x, y) { //supposed to be private now # does that I think
+        console.log(`Vertex drawn at (${x}, ${y})`);
+        this.ctx.beginPath();
+        this.ctx.arc(x, y, this.radius, 0, Math.PI*2, false);
+        this.ctx.lineWidth = 3;
+        this.ctx.strokeStyle = "#111"
+        this.ctx.fillStyle = "#111";
+        this.ctx.fill();
+        this.ctx.stroke();
+    }
 }
 window.onload = function() {
     const canvas = document.getElementById("theCanvas");
@@ -27,10 +53,11 @@ window.onload = function() {
     canvas.height = _height;
     canvas.width = _width;
     const ctx = canvas.getContext("2d");
-    vertices = new Array();
+    ctx.globalCompositeOperation = "screen";
+    const graph = new Graphy(7.5, ctx);
 
     canvas.addEventListener("click", (event) => {
-        placeVertex(event, vertices, ctx);
-        console.log(`Click registered at ( ${event.x}, ${event.y} ) OR ( ${event.offsetX}, ${event.offsetY} )`);
+        graph.clickCanvas(event);
+        //console.log(`Click registered at (${event.offsetX}, ${event.offsetY})`);
     });
 }
